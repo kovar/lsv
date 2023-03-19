9422c2bf67f00f97e961bbfcd5178ae6d526967e
Jiří Kovář <j.kovar@student.tudelft.nl>
2023-03-19 16:46:44 +0100

    small fix to the web build

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() {
         eframe::start_web(
             "the_canvas_id", // hardcode it
             web_options,
-            Box::new(|cc| Box::new(lsv::TemplateApp::new(cc))),
+            Box::new(|cc| Box::new(lsv::app::TemplateApp::new(cc))),
         )
         .await
         .expect("failed to start eframe");
